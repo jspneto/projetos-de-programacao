cbbc921cfa5f6d9eb55bf9cfe69159045b9a334d
José de Senna Pereira Neto <josespnt1990@gmail.com>
2025-12-06 22:08:55 -0300

    feat(f01-s03-csc-udemy): adiciona solucao do exercicio

---
+++ udemy/csc/s03_logicaProgramacao/f01_tiposDeDados.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Globalization;
+
+namespace S03_logicaProgramacao
+{
+    
+    internal class F01_tiposDeDados
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            string p1 = preco1.ToString("F2");
+            string p2 = preco2.ToString("F2");            
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é R$ {p1}");
+            Console.WriteLine($"{produto2}, cujo preço é R$ {p2}");
+            
+            Console.WriteLine();
+            Console.Write($"Registro: {idade} anos de idade,");
+            Console.WriteLine($" código {codigo} e gênero: {genero}");
+
+            string mOC = medida.ToString("F8");
+            string mAr = medida.ToString("F3");
+            string mDP = medida.ToString("F3", CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {mOC}");
+            Console.WriteLine($"Arredondado (três casas decimais): {mAr}");
+            Console.WriteLine($"Separador decimal invariant culture: {mDP}");
+
+        }
+
+    }
+
+}
