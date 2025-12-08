0b8d4f239be376e157dc0bf7edf4ff5e021cceeb
José de Senna Pereira Neto <josespnt1990@gmail.com>
2025-12-08 14:31:15 -0300

    feat(e01-ex01-s03-csc-udemy): adiciona solucao do exercicio

---
+++ udemy/csc/s03_logicaProgramacao/ex01_estruturaSequencial/e01_soma.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace s03_logicaProgramacao.ex01_estruturaSequencial
+{
+    
+    // Curso       : C# Completo
+    // Instrutor   : Nelio Alves
+    // Autor(a)    : José de Senna
+    
+    // Exercício 01 (Soma):
+    // 
+    // Faça um programa para ler dois valores inteiros, e depois mostre na tela
+    // a soma desses números com uma mensagem explicativa, conforme exemplos.
+
+    internal class E01_soma
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Programa para Somar Dois Números Inteiros");
+            Console.WriteLine();
+            Console.Write("# Digite o 1º número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("# Digite o 2º número: ");
+            int num2 = int.Parse(Console.ReadLine()!);
+
+            int soma = num1 + num2;
+
+            Console.WriteLine();
+            Console.WriteLine($"# A soma entre {num1} e {num2} é: {soma}.");
+
+        }
+
+    }
+
+}
