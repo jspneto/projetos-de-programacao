129008c5ff3a9c5c3b4556058e5b1fca1318f02b
José de Senna Pereira Neto <josespnt1990@gmail.com>
2025-12-08 20:47:46 -0300

    feat(e02-ex01-s03-csc-udemy): adiciona solucao do exercicio

---
+++ udemy/csc/s03_logicaProgramacao/ex01_estruturaSequencial/e02_circulo.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Globalization;
+
+namespace s03_logicaProgramacao.ex01_estruturaSequencial
+{
+    
+    // Curso       : C# Completo
+    // Instrutor   : Nelio Alves
+    // Autor(a)    : José de Senna
+    
+    // Exercício 02 (Círculo):
+    //
+    // Faça um programa para ler o valor do raio de um círculo, e depois
+    // mostre o valor da área deste círculo com quatro casas decimais,
+    // conforme exemplos.
+    //
+    // Fórmula da área: area = pi * raio²
+    //
+    // Considere o valor de pi = 3.14159
+
+    internal class E02_circulo
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Programa para Calcular a Área de um Círculo");
+            Console.WriteLine();
+            Console.WriteLine("# Digite a medida do raio: ");
+            string s = Console.ReadLine()!;
+            double raio = double.Parse(s, CultureInfo.InvariantCulture);
+
+            double PI = 3.14159;
+            double area = PI * Math.Pow(raio, 2);
+
+            string msgArea = area.ToString("F4", CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine($"# Área do Círculo: {msgArea} ua.");
+
+        }
+
+    }
+
+}
